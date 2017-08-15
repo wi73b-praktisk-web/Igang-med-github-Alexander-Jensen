51f38eaa2bc1e765d513ee1fb2f7979b352727a0
Alexander Lundegaard Jensen <3405636@RTS-365.dk>
2017-08-15 12:11:25 +0200

    upd

--- js1/opg2.js
+++ js1/opg2.js
@@ -1,9 +1,7 @@
-
-var pris ="20";
 var moms ="1.25";
 var pris_uden_moms="400";
 
-var pris_med_moms = pris * moms;
+var pris_med_moms = pris_uden_moms * moms;
 var pris_uden_moms2 = pris_med_moms / moms;
 
 console.log(pris_med_moms);
