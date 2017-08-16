2d72e0fdeb011ff2864668a9b984e737f4e7743c
Alexander Lundegaard Jensen <3405636@RTS-365.dk>
2017-08-16 08:11:42 +0200

    3

---
+++ js1/arraylist.js
@@ -0,0 +1,10 @@
+var cars = ["Saab", "Volvo", "BMW"];
+
+cars[0] = "Opel";
+
+console.log(cars[1]);
+console.log(cars.length);
+
+cars[cars.length] = "Mercedes";
+
+console.log(cars);
---
+++ js1/arrays.js
@@ -0,0 +1,15 @@
+var farve1 = "rød";
+var farve2 = "grøn";
+var farve3 = "blå";
+var farver = ["rød2", "blå2"];
+
+//console.log(farver);
+
+var spil = ["LoL", "WoW", "CSGO",
+    season = ["s1", "s2", "s3",
+    subgenre = ["333","444"]
+    ]
+];
+
+console.log(spil);
+
