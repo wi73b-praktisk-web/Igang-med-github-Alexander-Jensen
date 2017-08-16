16602dcf49b9e2f2798d2463532b772b2b5b0344
Alexander Lundegaard Jensen <3405636@RTS-365.dk>
2017-08-16 08:55:27 +0200

    4

---
+++ js1/nodeTest.js
@@ -0,0 +1,32 @@
+var respone = true;
+var navn = "Alexander";
+
+if(respone){
+    console.log("Its true");
+}else {
+    console.log("Its false");
+}
+
+if(true){
+//    console.log("Working");
+}
+
+if(navn == "Alexander"){
+
+}else if(navn == "Lund"){
+ 
+}   else {
+    console.log("");
+}
+
+var navn2 = "egon";
+switch(navn2){
+    case "Egon":
+    break;
+}
+console.log(navn2);
+
+var workHard = true;
+
+var result = workHard ? "Yep, work hard!" : "Nah im good";
+console.log(result);
